8a003992d253dfbe33cf65e5d326c77d77a3432a
aleksandra1dp <150381037+aleksandra1dp@users.noreply.github.com>
2026-04-11 14:33:12 +0100

    some more helper functions

--- sudoku_main.py
+++ sudoku_main.py
@@ -32,20 +32,44 @@ class Board:
         self.fixed = self.create_grid(False) #player is not allowed to change these cells if changed to True boolean array to track which cells are fixed
         self.solution = [] #copy of the completed grid to check against player's input
 
-    def row_valid(self, row: int, num: int, col_exclude: int | None = None) -> bool:
+    def row_valid(self, row: int, num: int, col_exclude: int = -1) -> bool:
         for c in range(self.size):
             if c == col_exclude:
                 continue
             if self.grid[row][c] == num: #if the number is found in the same row, it's not valid
                 return False
-        return True #othweise it's valid
+        return True #otherwise it's valid
     
-    def col_valid(self, col: int, num: int, row_exclude: int | None = None) -> bool:
+    def col_valid(self, col: int, num: int, row_exclude: int = -1) -> bool:
         for r in range(self.size):
             if r == row_exclude:
                 continue
             if self.grid[r][col] == num: #if the number is found in the same column, it's not valid
                 return False
-        return True #othweise it's valid
+        return True #otherwise it's valid
     
+    def box_valid(self, row: int, col: int, num: int, row_exclude: int = -1, col_exclude: int = -1) -> bool:
+        box_row_start = (row // self.base) * self.base # starting point example row 5 so 5/3=1 
+                                                        # then 1*3 = 3 so the top of the 3x3 box is 3
+        box_col_start = (col // self.base) * self.base
+        for r in range(box_row_start, box_row_start + self.base):
+            for c in range(box_col_start, box_col_start + self.base):
+                if (r, c) != (row_exclude, col_exclude): # skip the cell we are placing into -- don't conflict with itself
+                    if self.grid[r][c] == num: #if the number is found in the same 3x3 box, it's not valid
+                        return False
+        return True #otherwise it's valid
+    
+    def is_valid(self, row: int, col: int, num: int) -> bool: # checks for all three conditions for a valid placement
+        return self.row_valid(row, num, col) and self.col_valid(col, num, row) and self.box_valid(row, col, num, row, col)
+    
+    def is_cell_fixed(self, row: int, col: int) -> bool:
+        return self.fixed[row][col] #has a value that player cannot change
+    
+    def set_cell(self, row: int, col: int, num: int, fixed: bool = False):
+        self.grid[row][col] = num #sets the value of the cell to the number provided
+        self.fixed[row][col] = fixed #if fixed is True, player cannot change this cell
+
+    def get_cell(self, row: int, col: int) -> int:
+        return self.grid[row][col] #returns the value of the cell
+
 
