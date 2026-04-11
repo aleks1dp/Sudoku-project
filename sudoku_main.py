bb849747b5dc2a99c87258d1baa446291db8afbf
aleksandra1dp <150381037+aleksandra1dp@users.noreply.github.com>
2026-04-11 13:16:14 +0100

    initializtion

--- sudoku_main.py
+++ sudoku_main.py
@@ -1,4 +1,51 @@
-import random
-import time
-import copy
-import json
+import random #unique numbers each game
+import time #timer function of the game
+import copy #creates a copy for gameplay to retain the original solution
+import json #saves and loads game state
+import os #checks whether a save file exists
+from collections import deque #used for backtracking algorithm
+from datetime import datetime #used for timestamping save files
+
+class Colors:
+    BOLD = '\033[1m'
+    DIM = '\033[2m' # for empty cells to differentiate them from filled cells
+    RESET = '\033[0m' #changes back the color to original after
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+
+
+class Board:
+    size = 9
+    base = 3
+
+    #helper function for initialzing the grid and fixed arrays
+    def create_grid(self): 
+        return [[0 for  _ in range(self.size)] for _ in range(self.size)]
+
+    def __init__(self):
+        self.grid = self.create_grid(0) #empty grid of 9x9
+        self.fixed = self.create_grid(False) #player is not allowed to change these cells if changed to True boolean array to track which cells are fixed
+        self.solution = [] #copy of the completed grid to check against player's input
+
+    def row_valid(self, row: int, num: int, col_exclude: int | None = None) -> bool:
+        for c in range(self.size):
+            if c == col_exclude:
+                continue
+            if self.grid[row][c] == num: #if the number is found in the same row, it's not valid
+                return False
+        return True #othweise it's valid
+    
+    def col_valid(self, col: int, num: int, row_exclude: int | None = None) -> bool:
+        for r in range(self.size):
+            if r == row_exclude:
+                continue
+            if self.grid[r][col] == num: #if the number is found in the same column, it's not valid
+                return False
+        return True #othweise it's valid
+    
+
